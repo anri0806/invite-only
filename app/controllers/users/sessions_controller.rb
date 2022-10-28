6f7ea92e35bea93923c186a8e0eb7a66394541a6
Anri <anri0806@hotmail.co.jp>
2022-10-28 18:09:50 -0400

    added log out session on devise controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,9 +16,11 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+    session.delete :user_id
+    head :no_content
+  end
 
   # protected
 
