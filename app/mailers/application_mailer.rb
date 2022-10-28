e7f55e0cb88505350fcb4a125c0911e7d089ed9b
Anri <anri0806@hotmail.co.jp>
2022-10-28 16:15:19 -0400

    modified sender email in devise config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
-  # default from: 'no-reply@invite-only.com'
+  default from: 'no-reply@invite-only.com'
 
-  default from: 'Invite Only <no-reply@invite-only.com>'
+  # default from: 'Invite Only <no-reply@invite-only.com>'
   # default(from: "Invite Only <no-reply@invite-only.com>")
 
   
