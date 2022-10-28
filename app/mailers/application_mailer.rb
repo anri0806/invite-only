532aef85905a78f6cda8a5d558135f76bde124f3
Anri <anri0806@hotmail.co.jp>
2022-10-28 15:48:58 -0400

    modified default sender email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
   # default from: 'no-reply@invite-only.com'
-  # default from: email_address_with_name('no-reply@invite-only.com', 'Invite Only')
 
-  default(from: "Invite Only <no-reply@invite-only.com>")
+  default from: 'Invite Only <no-reply@invite-only.com>'
+  # default(from: "Invite Only <no-reply@invite-only.com>")
 
   
   layout 'mailer'
