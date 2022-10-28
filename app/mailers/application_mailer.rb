f65538523014b99aaa8439c14e5599f78c01ae9c
Anri <anri0806@hotmail.co.jp>
2022-10-28 13:18:26 -0400

    removed default email address with name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
-  # default from: 'no-reply@invite-only.com'
-  default from: email_address_with_name('no-reply@invite-only.com', 'Invite Only')
+  default from: 'no-reply@invite-only.com'
+  # default from: email_address_with_name('no-reply@invite-only.com', 'Invite Only')
 
   
   layout 'mailer'
