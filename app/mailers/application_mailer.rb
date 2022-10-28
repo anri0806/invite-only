5d292cda18ea47ce3dfa7be683b41960864b5529
Anri <anri0806@hotmail.co.jp>
2022-10-28 12:28:08 -0400

    added sent from email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
   default from: 'no-reply@invite-only.com'
-
+  
   layout 'mailer'
+
+  mail(from: 'Invite Only <no-reply@invite-only.com>')
 end
