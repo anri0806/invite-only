8bd2eaa85137e8d3e78a189a37a611b319a8fc93
Anri <anri0806@hotmail.co.jp>
2022-10-28 23:18:36 -0400

    added loading component

--- client/src/components/InviteForm.js
+++ client/src/components/InviteForm.js
@@ -6,15 +6,15 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Alert from "react-bootstrap/Alert";
+import LoadingSpinner from "./LoadingSpinner";
 
 function InviteForm() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState(null);
   const [showAlert, setShowAlert] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   const currentUser = useContext(UserContext);
-
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,23 +23,31 @@ function InviteForm() {
     formData.append("email", email);
     formData.append("group_id", currentUser.group_id);
 
+    setIsLoading(true);
+
     fetch("/users/invitation", {
       method: "POST",
       body: formData,
     }).then((res) => {
       if (res.ok) {
+        setIsLoading(false);
         setShowAlert((showAlert) => !showAlert);
         setError(null);
       } else {
-        res.json().then((err) => setError(err.error));
+        res.json().then((err) => {
+          setIsLoading(false);
+          setError(err.error);
+        });
       }
     });
 
     setEmail("");
+    setShowAlert(false);
   }
 
   return (
     <>
+      {isLoading ? <LoadingSpinner /> : null}
       {showAlert ? (
         <Alert variant="success">Invitation has been sent!</Alert>
       ) : null}
@@ -60,7 +68,11 @@ function InviteForm() {
             </Form.Group>
           </Col>
           <Col>
-            <Button variant="outline-secondary" type="submit">
+            <Button
+              variant="outline-secondary"
+              type="submit"
+              disabled={isLoading}
+            >
               Send
             </Button>
           </Col>
